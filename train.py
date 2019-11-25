d03b01cc701056237c1a4e5b0f377b219e2e8bd4
Lukas <lukas@uzolas.com>
2019-11-25 22:36:59 +0100

    create multiple models in one session

--- train.py
+++ train.py
@@ -1,21 +1,22 @@
 from perception_net import perception_net
 from prep_data import get_train_data, get_test_data
 from keras.optimizers import Adadelta
-from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+import keras.backend as K
 import pathlib
 import os
 import datetime as D
 
 # PerceptionNet parameters (channels last format)
-input_dim = (128, 6)
+input_dim = (6, 128)
 num_classes = 6
 
 # Hyper parameters
 batch_size = 64
-epochs = 2
+epochs = 2000
 validation_split = 0.15
 stopping_patience = 100
-model_save_frequency = 1
+model_save_frequency = 50
 
 # AdaDelta parameters
 learning_rate = 1.0
@@ -26,38 +27,41 @@ save_path = './run_data/'
 
 data, labels = get_train_data()
 test_data, test_labels = get_test_data()
-model = perception_net(input_dim, num_classes)
 
-session_path = os.path.join(save_path,  model.name + '_' + str(D.datetime.now()))
-weight_path = os.path.join(session_path, 'weights.{epoch:02d}-{val_loss:.4f}.hdf5')
-print(weight_path)
-pathlib.Path(session_path).mkdir(parents=True, exist_ok=True)
+for k in range(10):
+    model = perception_net(input_dim, num_classes)
 
+    session_path = os.path.join(save_path,  model.name + '_' + str(D.datetime.now()))
+    weight_path = os.path.join(session_path, 'weights.{epoch:02d}-{val_loss:.4f}.hdf5')
+    pathlib.Path(session_path).mkdir(parents=True, exist_ok=True)
 
-optimizer = Adadelta(lr=learning_rate,
-                     rho=rho,
-                     epsilon=epsilon)
-callbacks = [
-    EarlyStopping(patience=stopping_patience),
-    ModelCheckpoint(filepath=weight_path, period=model_save_frequency)
-]
+    optimizer = Adadelta(lr=learning_rate,
+                        rho=rho,
+                        epsilon=epsilon)
+    callbacks = [
+        EarlyStopping(patience=stopping_patience, restore_best_weights=True),
+        ModelCheckpoint(filepath=weight_path, period=model_save_frequency),
+        TensorBoard(log_dir=os.path.join(session_path, 'logs'), batch_size=64)
+    ]
 
-model.compile(loss="categorical_crossentropy",
-              optimizer=optimizer,
-              metrics=['accuracy'])
+    model.compile(loss="categorical_crossentropy",
+                optimizer=optimizer,
+                metrics=['accuracy'])
 
-model.fit(data,
-          labels,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_split=validation_split,
-          shuffle=True,
-          callbacks=callbacks)
+    model.fit(data,
+            labels,
+            batch_size=batch_size,
+            epochs=epochs,
+            validation_split=validation_split,
+            shuffle=True,
+            callbacks=callbacks)
 
-results = model.evaluate(test_data,
-                         test_labels,
-                         batch_size=batch_size)
+    results = model.evaluate(test_data,
+                            test_labels,
+                            batch_size=batch_size)
 
-print('test loss, test acc:', results)
+    print('test loss, test acc:', results)
 
-model.save_weights(filepath=os.path.join(session_path, 'weights.FINAL-{0:.4f}.hdf5'.format(results[0])))
+    model.save_weights(filepath=os.path.join(session_path, 'weights.FINAL-{0:.4f}.hdf5'.format(results[0])))
+
+    K.clear_session()
