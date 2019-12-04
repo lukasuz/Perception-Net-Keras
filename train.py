cd511617a31eb15a4eeb9067d48ceb42f46855dc
Lukas <lukas@uzolas.com>
2019-12-04 22:53:15 +0100

    functionify script

--- train.py
+++ train.py
@@ -1,37 +1,32 @@
-from perception_net import perception_net
-from prep_data import get_train_data, get_test_data
 from keras.optimizers import Adadelta
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 import keras.backend as K
 import pathlib
 import os
 import datetime as D
+import json
+import timeit
 
-# PerceptionNet parameters (channels last format)
-input_dim = (6, 128)
-num_classes = 6
+def train(model,
+          data,
+          labels,
+          weights,
+          test_data,
+          test_labels,
+          model_name,
+          # Hyper parameters
+          batch_size = 64,
+          epochs = 2000,
+          validation_split = 0.15,
+          stopping_patience = 100,
+          model_save_frequency = 50,
+          # AdaDelta parameters
+          learning_rate = 1.0,
+          rho = 0.95,
+          epsilon = 1e-08,
+          save_path = './run_data/'):
 
-# Hyper parameters
-batch_size = 64
-epochs = 2000
-validation_split = 0.15
-stopping_patience = 100
-model_save_frequency = 50
-
-# AdaDelta parameters
-learning_rate = 1.0
-rho = 0.95
-epsilon = 1e-08
-
-save_path = './run_data/'
-
-data, labels = get_train_data()
-test_data, test_labels = get_test_data()
-
-for k in range(10):
-    model = perception_net(input_dim, num_classes)
-
-    session_path = os.path.join(save_path,  model.name + '_' + str(D.datetime.now()))
+    session_path = os.path.join(save_path,  model_name + '_' + str(D.datetime.now()))
     weight_path = os.path.join(session_path, 'weights.{epoch:02d}-{val_loss:.4f}.hdf5')
     pathlib.Path(session_path).mkdir(parents=True, exist_ok=True)
 
@@ -45,22 +40,34 @@ for k in range(10):
     ]
 
     model.compile(loss="categorical_crossentropy",
-                optimizer=optimizer,
-                metrics=['accuracy'])
+                  optimizer=optimizer,
+                  metrics=['accuracy'])
 
+    start_time = D.datetime.now()
     model.fit(data,
-            labels,
-            batch_size=batch_size,
-            epochs=epochs,
-            validation_split=validation_split,
-            shuffle=True,
-            callbacks=callbacks)
+              labels,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_split=validation_split,
+              shuffle=True,
+              class_weight=weights,
+              callbacks=callbacks)
+    stop_time = D.datetime.now()
 
     results = model.evaluate(test_data,
                             test_labels,
                             batch_size=batch_size)
 
     print('test loss, test acc:', results)
+
+    run_data = {
+        'test_loss': results[0],
+        'test_acc': results[1],
+        'time': str(stop_time - start_time)
+    }
+    run_data_path = os.path.join(session_path, 'run_data.json')
+    with open(run_data_path, 'w') as file:
+        json.dump(run_data, file)
 
     model.save_weights(filepath=os.path.join(session_path, 'weights.FINAL-{0:.4f}.hdf5'.format(results[0])))
 
